0c77d1926cb1c571b681144b375984e9e0e45a74
ouijd <ouijdane.erraji@edu.uca.ma>
2021-07-16 11:33:35 +0100

    Initial commit

---
+++ src/main/java/ouijdane/shop/ouijdaneshop/OuijdaneshopApplication.java
@@ -0,0 +1,13 @@
+package ouijdane.shop.ouijdaneshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuijdaneshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OuijdaneshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ouijdane/shop/ouijdaneshop/OuijdaneshopApplicationTests.java
@@ -0,0 +1,13 @@
+package ouijdane.shop.ouijdaneshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuijdaneshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
